90dcb2a94390a270e925fbd7fdd07c8c6516ac95
Yuanfu Sun <103106931+sunyuanfu@users.noreply.github.com>
2026-04-08 20:10:35 +0800

    Delete OpenRLHF-RAG directory

--- OpenRLHF-RAG/run.sh
+++
@@ -1,4 +0,0 @@
-bash /PATH/TO/HOME/user/OpenRLHF/dpsk1_5b-1node-grpo-custom_1.sh | tee /PATH/TO/HOME/user/RAG_RL/results/log/dpsk1_5b-1node-grpo-custom-log_1
-sleep 360
-
-bash /PATH/TO/HOME/user/OpenRLHF/dpsk1_5b-1node-grpo-custom_2.sh | tee /PATH/TO/HOME/user/RAG_RL/results/log/dpsk1_5b-1node-grpo-custom-log_2
